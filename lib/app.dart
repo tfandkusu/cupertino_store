accbe097efff57f8692b3e6269d717e15de40e07
Haruhiko Takada <haruhiko.takada@gmail.com>
2020-06-07 22:21:05 +0900

    Add Japanese comments

--- lib/app.dart
+++ lib/app.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/cupertino.dart';
+// プラットフォームサービス。クリップボードやデバイス方向の設定ができる
 import 'package:flutter/services.dart';
 
 class CupertinoStoreApp extends StatelessWidget {
@@ -6,6 +7,8 @@ class CupertinoStoreApp extends StatelessWidget {
   Widget build(BuildContext context) {
     // This app is designed only to work vertically, so we limit
     // orientations to portrait up and down.
+    // servicesをimportしたら使える。
+    // 縦画面に固定する。
     SystemChrome.setPreferredOrientations(
         [DeviceOrientation.portraitUp, DeviceOrientation.portraitDown]);
 
@@ -18,7 +21,9 @@ class CupertinoStoreApp extends StatelessWidget {
 class CupertinoStoreHomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    // ナビゲーションバー付き画面
     return const CupertinoPageScaffold(
+      // ナビゲーションバーの設定
       navigationBar: CupertinoNavigationBar(
         middle: Text('Cupertino Store'),
       ),
