93d4c74d31a422ece5bd141b3308082a57d0cbdd
Haruhiko Takada <haruhiko.takada@gmail.com>
2020-06-07 23:46:24 +0900

    Add state management

--- lib/app.dart
+++ lib/app.dart
@@ -2,6 +2,10 @@ import 'package:flutter/cupertino.dart';
 // プラットフォームサービス。クリップボードやデバイス方向の設定ができる
 import 'package:flutter/services.dart';
 
+import 'product_list_tab.dart';
+import 'search_tab.dart';
+import 'shopping_cart_tab.dart';
+
 class CupertinoStoreApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
@@ -21,13 +25,53 @@ class CupertinoStoreApp extends StatelessWidget {
 class CupertinoStoreHomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    // ナビゲーションバー付き画面
-    return const CupertinoPageScaffold(
-      // ナビゲーションバーの設定
-      navigationBar: CupertinoNavigationBar(
-        middle: Text('Cupertino Store'),
+    // タブがある画面
+    return CupertinoTabScaffold(
+      // タブ一覧
+      tabBar: CupertinoTabBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(CupertinoIcons.home),
+            title: Text('Products'),
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(CupertinoIcons.search),
+            title: Text('Search'),
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(CupertinoIcons.shopping_cart),
+            title: Text('Cart'),
+          ),
+        ],
       ),
-      child: SizedBox(),
+      // 各タブのコンテンツ
+      tabBuilder: (context, index) {
+        CupertinoTabView returnValue;
+        switch (index) {
+          case 0:
+            returnValue = CupertinoTabView(builder: (context) {
+              return CupertinoPageScaffold(
+                child: ProductListTab(),
+              );
+            });
+            break;
+          case 1:
+            returnValue = CupertinoTabView(builder: (context) {
+              return CupertinoPageScaffold(
+                child: SearchTab(),
+              );
+            });
+            break;
+          case 2:
+            returnValue = CupertinoTabView(builder: (context) {
+              return CupertinoPageScaffold(
+                child: ShoppingCartTab(),
+              );
+            });
+            break;
+        }
+        return returnValue;
+      },
     );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,21 @@
 import 'package:flutter/cupertino.dart';
+import 'package:provider/provider.dart';
 import 'app.dart';
+import 'model/app_state_model.dart';
 
 void main() {
-  return runApp(CupertinoStoreApp());
+  return runApp(
+    ChangeNotifierProvider<AppStateModel>(
+      create: (_) {
+        AppStateModel stateModel = AppStateModel();
+        stateModel.loadProducts();
+        return stateModel;
+        // ラムダ式について
+        // 1行の時は=>
+        // オブジェクトに対してメソッドを呼び出すとき
+        // ..で呼び出すとCascade呼び出しになる。
+      },
+      child: CupertinoStoreApp(),
+    )
+  );
 }
