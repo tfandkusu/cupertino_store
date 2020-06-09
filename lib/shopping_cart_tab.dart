220a6afaa2e0e95bb58b13d36b6465d8e14f243d
Haruhiko Takada <haruhiko.takada@gmail.com>
2020-06-09 23:21:43 +0900

    Add selected items for purchase

--- lib/shopping_cart_tab.dart
+++ lib/shopping_cart_tab.dart
@@ -2,6 +2,7 @@ import 'package:flutter/cupertino.dart';
 import 'package:provider/provider.dart';
 import 'package:intl/intl.dart';
 import 'model/app_state_model.dart';
+import 'model/product.dart';
 import 'styles.dart';
 
 const double _kDateTimePickerHeight = 216;
@@ -19,6 +20,7 @@ class _ShoppingCartTabState extends State<ShoppingCartTab> {
   String location;
   String pin;
   DateTime dateTime = DateTime.now();
+  final _currencyFormat = NumberFormat.currency(symbol: '\$');
 
   @override
   Widget build(BuildContext context) {
@@ -46,7 +48,8 @@ class _ShoppingCartTabState extends State<ShoppingCartTab> {
   SliverChildBuilderDelegate _buildSliverChildBuilderDelegate(
       AppStateModel model) {
     return SliverChildBuilderDelegate(
-          (context, index) {
+      (context, index) {
+        final productIndex = index - 4;
         switch (index) {
           case 0:
             return Padding(
@@ -63,13 +66,56 @@ class _ShoppingCartTabState extends State<ShoppingCartTab> {
               padding: const EdgeInsets.symmetric(horizontal: 16),
               child: _buildLocationField(),
             );
-          case 3:                // ADD FROM HERE
+          case 3: // ADD FROM HERE
             return Padding(
               padding: const EdgeInsets.fromLTRB(16, 8, 16, 24),
               child: _buildDateAndTimePicker(context),
-            );                   // TO HERE
+            ); // TO HERE
           default:
-          // Do nothing. For now.
+            if (model.productsInCart.length > productIndex) {
+              // モデルからショッピングカートに入っている商品一覧を取得する
+              // 描画はShoppingCartItemにお任せ
+              return ShoppingCartItem(
+                index: index,
+                product: model.getProductById(
+                    model.productsInCart.keys.toList()[productIndex]),
+                quantity: model.productsInCart.values.toList()[productIndex],
+                lastItem: productIndex == model.productsInCart.length - 1,
+                formatter: _currencyFormat,
+              );
+            } else if (model.productsInCart.keys.length == productIndex &&
+                model.productsInCart.isNotEmpty) {
+              // 最終行では合計金額などを表示する
+              // それらの計算結果もモデルに入ってる。
+              return Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 20),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: <Widget>[
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.end,
+                      children: <Widget>[
+                        Text(
+                          'Shipping '
+                              '${_currencyFormat.format(model.shippingCost)}',
+                          style: Styles.productRowItemPrice,
+                        ),
+                        const SizedBox(height: 6),
+                        Text(
+                          'Tax ${_currencyFormat.format(model.tax)}',
+                          style: Styles.productRowItemPrice,
+                        ),
+                        const SizedBox(height: 6),
+                        Text(
+                          'Total  ${_currencyFormat.format(model.totalCost)}',
+                          style: Styles.productRowTotal,
+                        ),
+                      ],
+                    )
+                  ],
+                ),
+              );
+            }
         }
         // null返却で打ち止め
         return null;
@@ -150,7 +196,8 @@ class _ShoppingCartTabState extends State<ShoppingCartTab> {
     );
   }
 
-  Widget _buildDateAndTimePicker(BuildContext context) { // NEW FROM HERE
+  Widget _buildDateAndTimePicker(BuildContext context) {
+    // NEW FROM HERE
     return Column(
       children: <Widget>[
         Row(
@@ -191,5 +238,86 @@ class _ShoppingCartTabState extends State<ShoppingCartTab> {
         ),
       ],
     );
+  }
+}
+
+/// カートに入った商品1個分のViewを定義
+class ShoppingCartItem extends StatelessWidget {
+  const ShoppingCartItem({
+    @required this.index,
+    @required this.product,
+    @required this.lastItem,
+    @required this.quantity,
+    @required this.formatter,
+  });
+
+  final Product product;
+  final int index;
+  final bool lastItem;
+  final int quantity;
+  final NumberFormat formatter;
+
+  @override
+  Widget build(BuildContext context) {
+    final row = SafeArea(
+      top: false,
+      bottom: false,
+      child: Padding(
+        padding: const EdgeInsets.only(
+          left: 16,
+          top: 8,
+          bottom: 8,
+          right: 8,
+        ),
+        child: Row(
+          children: <Widget>[
+            ClipRRect(
+              borderRadius: BorderRadius.circular(4),
+              child: Image.asset(
+                product.assetName,
+                package: product.assetPackage,
+                fit: BoxFit.cover,
+                width: 40,
+                height: 40,
+              ),
+            ),
+            Expanded(
+              child: Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 12),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: <Widget>[
+                        Text(
+                          product.name,
+                          style: Styles.productRowItemName,
+                        ),
+                        Text(
+                          '${formatter.format(quantity * product.price)}',
+                          style: Styles.productRowItemName,
+                        ),
+                      ],
+                    ),
+                    const SizedBox(
+                      height: 4,
+                    ),
+                    Text(
+                      '${quantity > 1 ? '$quantity x ' : ''}'
+                          '${formatter.format(product.price)}',
+                      style: Styles.productRowItemPrice,
+                    )
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+
+    return row;
   }
 }
